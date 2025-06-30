fb2ab5d05910a6ab5d832749b469b4b99b0eff46
shrimpToWhale <2171180@hansung.ac.kr>
2025-06-30 10:57:35 +0900

    second commit

---
+++ src/screens/homescreen.js
@@ -0,0 +1,24 @@
+// src/screens/HomeScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🍜 FORK 홈 화면입니다</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fefefe',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
