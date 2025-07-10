c070a2ed32d97dc2915ab6442c1d077cc3d1f84f
shrimpToWhale <2171180@hansung.ac.kr>
2025-07-10 14:52:03 +0900

    로그인 테스트

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler'; // 반드시 최상단!
 import React from 'react';
 import { useFonts } from 'expo-font';
-//import HomeScreen from './src/screens/homescreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -20,6 +19,7 @@ import OrderDetailScreen from './src/screens/orderdetailscreen/OrderDetailScreen
 import RestaurantDetail from './src/screens/restaurantdetail/RestaurantDetail';
 import CartScreen from './src/screens/cartscreen/CartScreen';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import LoginScreen from "./src/screens/loginscreen/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -42,7 +42,7 @@ export default function App() {
       <SafeAreaView style={{ flex: 1 }}>
 
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator initialRouteName="LoginScreen">
             <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="KbotScreen" component={ChatBotScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Category" component={CategoryScreen} options={{ headerShown: false }} />
@@ -58,6 +58,7 @@ export default function App() {
             <Stack.Screen name="KbotSettings" component={KbotSettingsScreen} options={{ headerShown: false }} />
             <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} options={{ headerShown: false }} />
             <Stack.Screen name="CartScreen" component={CartScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
 
 
           </Stack.Navigator>
