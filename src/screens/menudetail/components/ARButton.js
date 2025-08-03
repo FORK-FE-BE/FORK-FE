54331fe69b6ac21bde188289c46994155ae8c6b8
yeeun <yeeun@yeeunui-MacBookAir.local>
2025-08-03 22:10:21 +0900

    ar 구현 방법 변경

--- src/screens/menudetail/components/ARButton.js
+++ src/screens/menudetail/components/ARButton.js
@@ -1,13 +1,31 @@
-// src/screens/menuDetail/components/ARButton.js
+// // src/screens/menuDetail/components/ARButton.js
+// import React from 'react';
+// import { TouchableOpacity, Text } from 'react-native';
+// import * as WebBrowser from 'expo-web-browser';
+// import styles from './styles';
+
+// export default function ARButton({ url }) {
+//     return (
+//         <TouchableOpacity style={styles.arButton} onPress={() => WebBrowser.openBrowserAsync(url)}>
+//             <Text style={styles.arButtonText}>AR로 보기</Text>
+//         </TouchableOpacity>
+//     );
+// }
+
+
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import styles from './styles';
 
 export default function ARButton({ url }) {
-    return (
-        <TouchableOpacity style={styles.arButton} onPress={() => WebBrowser.openBrowserAsync(url)}>
-            <Text style={styles.arButtonText}>AR로 보기</Text>
-        </TouchableOpacity>
-    );
+  const handleOpen = () => {
+    WebBrowser.openBrowserAsync(url);
+  };
+
+  return (
+    <TouchableOpacity style={styles.arButton} onPress={handleOpen}>
+      <Text style={styles.arButtonText}>AR로 보기</Text>
+    </TouchableOpacity>
+  );
 }
