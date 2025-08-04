82d3c809b6150a1b9d3bf45a2be1cf56329f9f66
shrimpToWhale <2171180@hansung.ac.kr>
2025-08-04 15:27:54 +0900

    채팅 응답 수정

--- src/screens/chatbotscreen/ChatBotScreen.js
+++ src/screens/chatbotscreen/ChatBotScreen.js
@@ -6,19 +6,30 @@ import ChatInputBox from './components/ChatInputBox';
 import ChatBody from './components/ChatBody';
 import axios from 'axios';
 import {BASE_URL} from "../../constants";
+import {useUser} from "../../contexts/UserContext";
 
 export default function ChatBotScreen() {
   const [messages, setMessages] = useState([
     { id: '1', sender: 'bot', text: '안녕하세요, ○○님!\n저는 AI 크봇이에요.\n원하시는 게 있다면 말씀해주세요!' }
   ]);
+  const {user} = useUser();
   // 서버 통신 및 메시지 추가 함수
   const sendMessageToServer = async (userMessage) => {
     setMessages(prev => [...prev, { id: String(Date.now()), sender: 'user', text: userMessage }]);
 
     try {
-      const response = await axios.post(`${BASE_URL}/api/chatbot`, { message: userMessage });
+      const response = await axios.post(`${BASE_URL}/api/chatbot`, {
+        message: userMessage,
+        userId: user.userId
+      });
       const chatbotResponse = response.data.response;
+      const restaurants = response.data.recommendation;
+      const suggestions = response.data.followUpSuggestions;
+
       console.log(chatbotResponse);
+      console.log(restaurants);
+      console.log(suggestions);
+
       setMessages(prev => [...prev, { id: String(Date.now() + 1), sender: 'bot', text: chatbotResponse }]);
     } catch (error) {
       setMessages(prev => [...prev, { id: String(Date.now() + 2), sender: 'bot', text: '서버와 통신 중 오류가 발생했습니다.' }]);
